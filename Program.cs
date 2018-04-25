5de9fd58d23837e6f4e80cef9a6fbc093a743c06
Unknown <marioturtwelve@gmail.com>
2018-04-25 21:06:28 +0200

    1.0.0
    Insert files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IANews
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
